fa5c4eeef40485c40f94041db3153c6f29676f2a
Svetlana Kudryavtseva <springinmypalms@gmail.com>
2022-12-08 15:36:03 +0300

    deletion of temporary BYU button

--- [18293] Внесение правок по ТЗ/[] Кнопка в просмотренных товарах/catalog.item_seonik/templates/default/card/template.php
+++ [18293] Внесение правок по ТЗ/[] Кнопка в просмотренных товарах/catalog.item_seonik/templates/default/card/template.php
@@ -334,13 +334,7 @@ use \Bitrix\Main\Localization\Loc;
 										id="<?=$itemIds['NOT_AVAILABLE_MESS']?>" href="javascript:void(0)" rel="nofollow">
 										<?=$arParams['MESS_NOT_AVAILABLE']?>
 									</a>
-									<div id="<?=$itemIds['BASKET_ACTIONS']?>" <?=($actualItem['CAN_BUY'] ? '' : 'style="display: block;"')?>>
-										<a class="btn btn-default <?=$buttonSizeClass?>" id="<?=$itemIds['BUY_LINK']?>"
-											href="javascript:void(0)" rel="nofollow">
-											<?=($arParams['ADD_TO_BASKET_ACTION'] === 'BUY' ? $arParams['MESS_BTN_BUY'] : $arParams['MESS_BTN_ADD_TO_BASKET'])?>
-										</a>
-									</div>
-								</div>
+
 								<?
 							}
 						}
